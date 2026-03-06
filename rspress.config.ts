6d71bbbf2072173b3815fc068a432b646c453f2c
sampoi <samuel.poirier@beslogic.com>
2026-03-06 15:38:18 -0500

    Add title config and home root link

--- rspress.config.ts
+++ rspress.config.ts
@@ -5,6 +5,7 @@ const base = process.env.RS_PRESS_BASE || '/';
 export default defineConfig({
   root: 'docs',
   base: base,
+  title: 'MixMaster WECS User Manual',
   // Ensure assets load correctly in production
   builderConfig: {
     output: {
